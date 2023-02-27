091891ce58d3a3e4e3bc179c2421531edcfe1214
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-02-27 15:46:04 +0100

    eMail answers added

--- mail/js/main.js
+++ mail/js/main.js
@@ -2,10 +2,16 @@ const eMail = prompt("inserisci la tua e-mail");
 
 const eMailList = ["primamail@gmail.com", "secondamail@gmail.com", "terzamail@gmail.com", "quartamail@gmail.com", "quintamail@gmail.com", "sestamail@gmail.com"];
 
+let mySwitch = false;
+
 for (let i = 0; i < eMailList.length; i++) {
     if (eMail == eMailList[i]) {
-        console.log("accesso consentito")
-    } /*else {
-        console.log("errore di digitazione")
-    }*/
+        mySwitch = true;   
+    }
+}
+
+if (mySwitch == true) {
+    console.log("accesso consentito")
+} else {
+    console.log("errore di digitazione")  
 }
