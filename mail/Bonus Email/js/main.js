037292ea3b869209ce62a5a37ac8e324c9c2472d
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-02-27 17:10:16 +0100

    added answer

--- mail/Bonus Email/js/main.js
+++ mail/Bonus Email/js/main.js
@@ -1,6 +1,6 @@
 let avvioForm = document.querySelector(".submit");
 
-const eMail = document.getElementById("eMail");
+const eMailInput = document.getElementById("eMailInput");
 
 
 const eMailList = ["primamail@gmail.com", "secondamail@gmail.com", "terzamail@gmail.com", "quartamail@gmail.com", "quintamail@gmail.com", "sestamail@gmail.com"];
@@ -10,19 +10,26 @@ let mySwitch = false;
 avvioForm.addEventListener("click",
     function () {
         
+        let eMailValue = eMailInput.value;
 
         for (let i = 0; i < eMailList.length; i++) {
-            if (eMail == eMailList[i]) {
+            if (eMailValue == eMailList[i]) {
                 mySwitch = true;   
             }
         }
         
         if (mySwitch == true) {
             console.log("accesso consentito")
-            console.log(eMail);
+            const answer = document.querySelector(".answer");
+            answer.classList.remove("d_none");
+            answer.append("accesso consentito");
+            console.log(eMailValue);
         } else {
             console.log("errore di digitazione")
-            console.log(eMail);
+            const answer = document.querySelector(".answer");
+            answer.classList.remove("d_none");
+            answer.append("errore di digitazione");
+            console.log(eMailValue);
         }
         
     }
