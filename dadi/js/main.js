6827513f32b7a58533c3a7735cddd1a4757353d4
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-02-27 17:30:57 +0100

    styled css + draw

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -5,7 +5,27 @@ let randomPlayer = Math.floor(Math.random() * 6 ) + 1;
 console.log(randomPlayer);
 
 if (randomPc < randomPlayer) {
-    console.log("Player has won!")
+    console.log("Player has won!");
+    const answer = document.querySelector(".answer");
+    answer.classList.remove("d_none");
+    answer.append("Vince Il Player!");
 } else if (randomPc > randomPlayer) {
-    console.log("Computer has won!")
+    console.log("Computer has won!");
+    const answer = document.querySelector(".answer");
+    answer.classList.remove("d_none");
+    answer.append("Vince Il Computer!");
+} else {
+    console.log("Pareggio!");
+    const answer = document.querySelector(".answer");
+    answer.classList.remove("d_none");
+    answer.append("Pareggio!");
 }
+
+const score = document.querySelector(".score");
+score.classList.remove("d_none");
+score.append(
+`
+    Pc-score: ${randomPc}
+    Player-score:${randomPlayer} 
+`
+);
